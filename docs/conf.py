f215e9c9ec83384c91f685199b0bbd0487430ad9
Daichi Yamazaki <34172442+euphoria108@users.noreply.github.com>
2022-01-30 18:11:36 +0900

    Add docs and workflow file (#7)

---
+++ docs/conf.py
@@ -0,0 +1,116 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import logging
+import os
+import sys
+
+import opthub_scorer
+
+sys.path.insert(0, os.path.abspath("../"))
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+# -- Project information -----------------------------------------------------
+
+project = "Opthub Scorer"
+copyright = "2020 Naoki Hamada"
+author = "Naoki Hamada"
+
+# The full version, including alpha/beta/rc tags
+release = version = opthub_scorer.__version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+]
+
+# -- autodoc -----------------------------------------------------------------
+autodoc_type_aliases = {}
+autodoc_inherit_docstrings = True
+
+# -- autosectionlabel --------------------------------------------------------
+autosectionlabel_prefix_document = True
+
+# -- doctest -----------------------------------------------------------------
+# https://www.sphinx-doc.org/ja/master/usage/extensions/doctest.html#confval-doctest_global_setup
+doctest_global_setup = """
+import opthub_scorer
+"""
+doctest_test_doctest_blocks = "default"
+
+# -- intersphinx -------------------------------------------------------------
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+
+# https://www.sphinx-doc.org/ja/master/usage/extensions/linkcode.html
+# Resolve function for the linkcode extension.
+def linkcode_resolve(domain, info):
+    def find_source():
+        # try to find the file and line number, based on code from numpy:
+        # https://github.com/numpy/numpy/blob/master/doc/source/conf.py#L286
+        obj = sys.modules[info["module"]]
+        for part in info["fullname"].split("."):
+            obj = getattr(obj, part)
+        import inspect
+        import os
+
+        fn = inspect.getsourcefile(obj)
+        fn = os.path.relpath(fn, start=os.path.dirname(opthub_scorer.__file__))
+        source, lineno = inspect.getsourcelines(obj)
+        return fn, lineno, lineno + len(source) - 1
+
+    if domain != "py" or not info["module"]:
+        return None
+    try:
+        filename = "opthub_scorer/%s#L%d-L%d" % find_source()
+    except Exception as e:
+        logger.warning(e)
+        filename = info["module"].replace(".", "/") + ".py"
+    tag = "master"  # if 'dev' in release else ('v' + release)
+    return "https://github.com/opthub-org/opthub-scorer/blob/%s/%s" % (tag, filename)
